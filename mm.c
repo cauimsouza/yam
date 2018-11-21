3b5b4576366786de9f0857e1f7aaca6169fb94aa
Cauim de S. Lima <cauimsouza@gmail.com>
2018-11-21 17:23:27 +0100

    Remove footer from allocated blocks
    This modification intends to make better use of memory
    by decreasing memory overhead due to the usage of
    a header/footer in each allocated block.
    
    Footer are removed from allocated blocks and contain only
    a header. Free blocks still contain a header and a footer.
    
    As a consequence, the minimum block size decreases from
    16 bytes to 8 bytes, and a block only "knows" the size of
    its previous block if this previous block is currently free.

--- mm.c
+++ mm.c
@@ -35,24 +35,21 @@ team_t team = {
     "ian.duleba@polytechnique.edu"
 };
 
-/* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+/* Minimum block size */
+#define MIN_BLK_SIZE 8
 
-#define MIN_BLK_SIZE 16
+/* Single word size */
 #define WSIZE 4
+
+/* Double word size */
 #define DSIZE 8
 
-/* Returns the maximum of two integers */
+/* Returns the maximum/minimum of two integers */
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) > (y) ? (y) : (x))
 
-/* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
-
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
-
-/* Pack a size and allocated bit into a word */
-#define PACK(size, alloc) ((size) | (alloc))
+/* Pack a size, previous_allocated bit and allocated bit into a word */
+#define PACK(size, prev_alloc, alloc) ((size) | ((prev_alloc) << 1) | (alloc))
 
 /* Read and write a word at adress p */
 #define GET(p)		(*(unsigned int *)(p))
@@ -61,6 +58,9 @@ team_t team = {
 /* Read the size and allocated fields from address p */
 #define GET_SIZE(p)		(GET(p) & ~0x7)
 #define GET_ALLOC(p)	(GET(p) & 0x1)
+
+/* Return 1 iff PINUSE_BIT field from address p is set */
+#define GET_PALLOC(p) (!!(GET(p) & 0x2))
 
 /* Given block ptr bp, compute address of its header and footer */
 #define HDRP(bp) ((char *) (bp) - WSIZE)
@@ -84,16 +84,16 @@ static void *find_fit(size_t asize);
  */
 int mm_init(void)
 {
-	if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
-		return -1;
+	if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1) return -1;
 	PUT(heap_listp, 0);									/* Alignment padding */
-	PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));		/* Prologue header */
-	PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));		/* Prologue footer */
-	PUT(heap_listp + (3 * WSIZE), PACK(0, 1));			/* Epilogue header */
-	heap_listp += (2 * WSIZE);
+	PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1, 1));	/* Prologue header */
+	PUT(heap_listp + (3 * WSIZE), PACK(0, 1, 1));		/* Epilogue header */
+	heap_listp += DSIZE;
 
-	if (extend_heap(mem_pagesize() / WSIZE) == NULL)
-		return -1;
+	if (extend_heap(mem_pagesize() / WSIZE) == NULL) return -1;
+
+	printf("INTIALIZED WITH SIZE %d\n", GET_SIZE(HDRP(NEXT_BLKP(heap_listp))));
+	printf("End of heap: %p\n\n", NEXT_BLKP(NEXT_BLKP(heap_listp)));
 
     return 0;
 }
@@ -105,13 +105,16 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
+#ifdef DEBUG
+	printf("Calling malloc with size = %d\n", size);
+#endif
 	size_t asize;			/* Adjusted block size */
 	size_t extendsize;		/* Amount to extend heap if no fit */
     char *bp;
 
 	if (size == 0) return NULL;
 
-	if (size <= DSIZE) asize = 2 * DSIZE;
+	if (size <= DSIZE) asize = DSIZE;
 	else asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
 
     /* Search the free list for a fit */
@@ -121,22 +124,38 @@ void *mm_malloc(size_t size)
 	}
 
 	extendsize = MAX(asize, mem_pagesize());
-	if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
-		return NULL;
+	if ((bp = extend_heap(extendsize / WSIZE)) == NULL) return NULL;
 	place(bp, asize);
+#ifdef DEBUG
+	printf("\tCreated new block at %p with %d bytes\n\n", bp, GET_SIZE(HDRP(bp)));
+#endif
 	return bp;
 }
 
 /*
- * mm_free - Freeing a block does nothing.
+ * mm_free - Updates header and footer and coalesce with neighbors
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
-	size_t size = GET_SIZE(HDRP(ptr));
+	size_t size = GET_SIZE(HDRP(bp));
+	size_t prev_alloc = GET_PALLOC(HDRP(bp));
+#ifdef DEBUG
+	printf("Calling mm_free with bp = %p\n", bp);
+	printf("\tblock size = %d bytes and PINUSE_BIT = %d\n", size, prev_alloc);
+#endif
 
-	PUT(HDRP(ptr), PACK(size, 0));
-	PUT(FTRP(ptr), PACK(size, 0));
-	coalesce(ptr);
+	PUT(HDRP(bp), PACK(size, prev_alloc, 0));
+	PUT(FTRP(bp), PACK(size, prev_alloc, 0));
+
+	char *next_bp = NEXT_BLKP(bp);
+	size_t next_size = GET_SIZE(HDRP(next_bp));
+	size_t next_alloc = GET_ALLOC(HDRP(next_bp));
+	PUT(HDRP(next_bp), PACK(next_size, 0, next_alloc));
+
+#ifdef DEBUG
+	printf("\t%d bytes freed\n\n", GET_SIZE(HDRP(bp)));
+#endif
+	coalesce(bp);
 }
 
 /*
@@ -149,11 +168,9 @@ void *mm_realloc(void *ptr, size_t size)
     size_t copySize;
     
     newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
+    if (newptr == NULL) return NULL;
+
+    copySize = MIN(size, GET_SIZE(HDRP(ptr)));
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
@@ -165,6 +182,9 @@ void *mm_realloc(void *ptr, size_t size)
  */
 static void *extend_heap(size_t words)
 {
+#ifdef DEBUG
+	printf("Call to extend heap with words = %d\n", words);
+#endif
 	char *bp;
 	size_t size;
 
@@ -174,20 +194,31 @@ static void *extend_heap(size_t words)
 		return NULL;
 
 	/* Initialize free block header/footerand the epilogue header */
-	PUT(HDRP(bp), PACK(size, 0));				/* Free block header */
-	PUT(FTRP(bp), PACK(size, 0));				/* Free block footer */
-	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));		/* New epilogue header */
+	size_t prev_alloc = GET_PALLOC(HDRP(bp));
+	PUT(HDRP(bp), PACK(size, prev_alloc, 0));		/* Free block header */
+	PUT(FTRP(bp), PACK(size, prev_alloc, 0));		/* Free block footer */
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 0, 1));		/* New epilogue header */
+#ifdef DEBUG
+	printf("\tnew block created with %d bytes\n\n", size);
+#endif
 
 	/* Coalesce if the previous block was free */
 	return coalesce(bp);
 }
 
 
+/*
+ * coalesce - Coalesce block pointed by bp with its neighbors
+ */
 static void *coalesce(void *bp)
 {
-	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t prev_alloc = GET_PALLOC(HDRP(bp));
 	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
 	size_t size = GET_SIZE(HDRP(bp));
+#ifdef DEBUG
+	printf("Called coalesce with bp = %p\n", bp);
+	printf("\tPINUSE_BIT = %d, NINUSE = %d, size = %d\n\n", prev_alloc, next_alloc, size);
+#endif
 
 	if (prev_alloc && next_alloc) {
 		/* Do nothing */
@@ -195,21 +226,22 @@ static void *coalesce(void *bp)
 	
 	else if (prev_alloc && !next_alloc) {
 		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-		PUT(HDRP(bp), PACK(size, 0));
-		PUT(FTRP(bp), PACK(size, 0));
+		PUT(HDRP(bp), PACK(size, 1, 0));
+		PUT(FTRP(bp), PACK(size, 1, 0));
 	}
 	
 	else if (!prev_alloc && next_alloc) {
 		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-		PUT(FTRP(bp), PACK(size, 0));
-		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		/* The block before 'previous' is always in use */
+		PUT(FTRP(bp), PACK(size, 1, 0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 1, 0));
 		bp = PREV_BLKP(bp);
 	}
 	
 	else {
 		size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
-		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 1, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 1, 0));
 		bp = PREV_BLKP(bp);
 	}
 
@@ -219,35 +251,72 @@ static void *coalesce(void *bp)
 
 
 
+/*
+ * find_fit - Returns first free block with enough capacity
+ */
 static void *find_fit(size_t asize)
 {
+#ifdef DEBUG
+	printf("Called find_fit with asize = %d\n", asize);
+	int cunt = 1;
+#endif
 	char *bp;
 	for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-		if (GET_SIZE(HDRP(bp)) >= asize && !GET_ALLOC(HDRP(bp)))
+		if (GET_SIZE(HDRP(bp)) >= asize && !GET_ALLOC(HDRP(bp))) {
+#ifdef DEBUG
+			printf("\tfound free block with %d bytes in the %d iteration at address %p\n\n"\
+					, GET_SIZE(HDRP(bp)), cunt, bp);
+#endif
 			return bp;
+		}
+#ifdef DEBUG
+		cunt++;
+#endif
 	}
-
+#ifdef DEBUG
+			printf("\tdidn't find any free block\n\n");
+#endif
 	return NULL;
 }
 
 
 
+/*
+ * place - Place a new block of size at least asize in block
+ * pointed by bp
+ */
 static void place(void *bp, size_t asize)
 {
 	size_t size = GET_SIZE(HDRP(bp));
+	size_t prev_alloc = GET_PALLOC(HDRP(bp));
+#ifdef DEBUG
+	printf("Calling place with bp = %p and asize = %d\n", bp, asize);
+	printf("\tcurrent block has size = %d bytes and PINSUSE_BIT = %d\n", size, prev_alloc);
+#endif
 
 	if (size >= asize + MIN_BLK_SIZE) {
-		PUT(HDRP(bp), PACK(asize, 1));
-		PUT(FTRP(bp), PACK(asize, 1));
+		PUT(HDRP(bp), PACK(asize, prev_alloc, 1));
 
 		char *next_bp = NEXT_BLKP(bp);
-		PUT(HDRP(next_bp), PACK(size - asize, 0));
-		PUT(FTRP(next_bp), PACK(size - asize, 0));
+		PUT(HDRP(next_bp), PACK(size - asize, 1, 0));
+		PUT(FTRP(next_bp), PACK(size - asize, 1, 0));
+#ifdef DEBUG
+	printf("\tfirst block with %d bytes, second block with %d bytes\n", \
+			GET_SIZE(HDRP(bp)), GET_SIZE(HDRP(next_bp)));
+	printf("\tsecond at adress %p, PINUSE_BIT = %d\n\n", next_bp, GET_PALLOC(HDRP(next_bp)));
+#endif
 	}
 
 	else {
-		PUT(HDRP(bp), PACK(size, 1));
-		PUT(FTRP(bp), PACK(size, 1));
+		PUT(HDRP(bp), PACK(size, prev_alloc, 1));
+
+		char *next_bp = NEXT_BLKP(bp);
+		size_t next_size = GET_SIZE(HDRP(next_bp));
+		size_t next_alloc = GET_ALLOC(HDRP(next_bp));
+		PUT(HDRP(next_bp), PACK(next_size, 1, next_alloc));
+#ifdef DEBUG
+	printf("\tonly one block with %d bytes\n\n", GET_SIZE(HDRP(bp)));
+#endif
 	}
 }
 
